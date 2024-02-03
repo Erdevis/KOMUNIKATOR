c43b59093897091d3c108a75700fc61805e3e402
Erdevis <bartek.natkaniec@vp.pl>
2024-02-03 22:13:58 +0100

    a

--- KOMUNIKATOR-QSQLITE/debug/moc_programwindow.cpp
+++ KOMUNIKATOR-QSQLITE/debug/moc_programwindow.cpp
@@ -22,7 +22,7 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_ProgramWindow_t {
-    uint offsetsAndSizes[18];
+    uint offsetsAndSizes[26];
     char stringdata0[14];
     char stringdata1[15];
     char stringdata2[1];
@@ -32,6 +32,10 @@ struct qt_meta_stringdata_ProgramWindow_t {
     char stringdata6[22];
     char stringdata7[12];
     char stringdata8[12];
+    char stringdata9[27];
+    char stringdata10[17];
+    char stringdata11[5];
+    char stringdata12[24];
 };
 #define QT_MOC_LITERAL(ofs, len) \
     uint(sizeof(qt_meta_stringdata_ProgramWindow_t::offsetsAndSizes) + ofs), len 
@@ -45,7 +49,11 @@ static const qt_meta_stringdata_ProgramWindow_t qt_meta_stringdata_ProgramWindow
         QT_MOC_LITERAL(52, 21),  // "on_znajomiBtn_clicked"
         QT_MOC_LITERAL(74, 21),  // "on_connectBtn_clicked"
         QT_MOC_LITERAL(96, 11),  // "readMessage"
-        QT_MOC_LITERAL(108, 11)   // "onConnected"
+        QT_MOC_LITERAL(108, 11),  // "onConnected"
+        QT_MOC_LITERAL(120, 26),  // "on_friendsList_itemClicked"
+        QT_MOC_LITERAL(147, 16),  // "QListWidgetItem*"
+        QT_MOC_LITERAL(164, 4),  // "item"
+        QT_MOC_LITERAL(169, 23)   // "on_disconectBtn_clicked"
     },
     "ProgramWindow",
     "updateUserList",
@@ -55,7 +63,11 @@ static const qt_meta_stringdata_ProgramWindow_t qt_meta_stringdata_ProgramWindow
     "on_znajomiBtn_clicked",
     "on_connectBtn_clicked",
     "readMessage",
-    "onConnected"
+    "onConnected",
+    "on_friendsList_itemClicked",
+    "QListWidgetItem*",
+    "item",
+    "on_disconectBtn_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -65,7 +77,7 @@ static const uint qt_meta_data_ProgramWindow[] = {
       10,       // revision
        0,       // classname
        0,    0, // classinfo
-       6,   14, // methods
+       8,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -73,12 +85,14 @@ static const uint qt_meta_data_ProgramWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    1,   50,    2, 0x0a,    1 /* Public */,
-       4,    0,   53,    2, 0x08,    3 /* Private */,
-       5,    0,   54,    2, 0x08,    4 /* Private */,
-       6,    0,   55,    2, 0x08,    5 /* Private */,
-       7,    0,   56,    2, 0x08,    6 /* Private */,
-       8,    0,   57,    2, 0x08,    7 /* Private */,
+       1,    1,   62,    2, 0x0a,    1 /* Public */,
+       4,    0,   65,    2, 0x08,    3 /* Private */,
+       5,    0,   66,    2, 0x08,    4 /* Private */,
+       6,    0,   67,    2, 0x08,    5 /* Private */,
+       7,    0,   68,    2, 0x08,    6 /* Private */,
+       8,    0,   69,    2, 0x08,    7 /* Private */,
+       9,    1,   70,    2, 0x08,    8 /* Private */,
+      12,    0,   73,    2, 0x08,   10 /* Private */,
 
  // slots: parameters
     QMetaType::Void, QMetaType::QStringList,    3,
@@ -86,6 +100,8 @@ static const uint qt_meta_data_ProgramWindow[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 10,   11,
     QMetaType::Void,
 
        0        // eod
@@ -103,6 +119,8 @@ void ProgramWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _i
         case 3: _t->on_connectBtn_clicked(); break;
         case 4: _t->readMessage(); break;
         case 5: _t->onConnected(); break;
+        case 6: _t->on_friendsList_itemClicked((*reinterpret_cast< std::add_pointer_t<QListWidgetItem*>>(_a[1]))); break;
+        case 7: _t->on_disconectBtn_clicked(); break;
         default: ;
         }
     }
@@ -116,7 +134,7 @@ const QMetaObject ProgramWindow::staticMetaObject = { {
     nullptr,
 qt_incomplete_metaTypeArray<qt_meta_stringdata_ProgramWindow_t
 , QtPrivate::TypeAndForceComplete<ProgramWindow, std::true_type>
-, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<const QStringList &, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
+, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<const QStringList &, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<QListWidgetItem *, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
 
 
 >,
@@ -143,13 +161,13 @@ int ProgramWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 6)
+        if (_id < 8)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 6;
+        _id -= 8;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 6)
+        if (_id < 8)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 6;
+        _id -= 8;
     }
     return _id;
 }
